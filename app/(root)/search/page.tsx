24e39d2fe7743b00ab21fa139eeb5a40b6d10be7
Sanjeev <sanjeevkansyakar789@gmail.com>
2023-08-31 13:44:30 +0530

    first commit

---
+++ app/(root)/search/page.tsx
@@ -0,0 +1,62 @@
+import ProfileHeader from "@/components/shared/ProfileHeader";
+import ThreadsTab from "@/components/shared/ThreadsTab";
+import UserCard from "@/components/cards/UserCard";
+
+import { fetchUser, fetchUsers } from "@/lib/actions/user.action";
+
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import Image from "next/image";
+
+import { profileTabs } from "@/constants";
+
+async function Page() {
+  const user = await currentUser();
+  if (!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
+
+  //   Fetch users
+  const result = await fetchUsers({
+    userId: user.id,
+    searchString: "",
+    pageNumber: 1,
+    //   searchString: searchParams.q,
+    //   pageNumber: searchParams?.page ? +searchParams.page : 1,
+    pageSize: 25,
+  });
+
+  return (
+    <section>
+      <h1 className="head-text mb-10">Search</h1>
+
+      <div className="mt-14 flex flex-col gap-9">
+        {result.users.length === 0 ? (
+          <p className="no-result">No Result</p>
+        ) : (
+          <>
+            {result.users.map((person) => (
+              <UserCard
+                key={person.id}
+                id={person.id}
+                name={person.name}
+                username={person.username}
+                imgUrl={person.image}
+                personType="User"
+              />
+            ))}
+          </>
+        )}
+      </div>
+
+      {/* <Pagination
+        path="search"
+        pageNumber={searchParams?.page ? +searchParams.page : 1}
+        isNext={result.isNext}
+      /> */}
+    </section>
+  );
+}
+
+export default Page;
